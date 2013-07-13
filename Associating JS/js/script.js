5140d6eecf4ffac979233c1f9ca6db50cec25ec2
Bigdsweetz <bigdsweetz@gmail.com>
2013-07-13 17:34:18 -0400

    This is my Values and Vaiables group. I've completed all of the following and pretty much just left all the call's in. If I don't need to use them I'll just comment them out. I'm sure I'll have to use them again in the future.

---
+++ Associating JS/js/script.js
@@ -0,0 +1,16 @@
+//JAVASCRIPT
+
+var yearBorn = 1955;
+console.log(yearBorn);
+alert("The meaning a life");
+
+
+var age; //declaration
+    age = 42; //defining
+alert(age); //alert to show variable
+
+var firstName = "DJay";
+var phrase = " I \n don't \n know";
+var muppetNames = ["1","2","3", firstName ];
+
+console.log(muppetNames[3]);
