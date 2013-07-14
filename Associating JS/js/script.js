a3f0120574da20d99df57a9c9cc92350de4f4df1
Bigdsweetz <bigdsweetz@gmail.com>
2013-07-13 23:39:27 -0400

    I've have also finished the Expressions group. This is the overall group activity that has happened here. Also
    [16:37:02.178] The character encoding of the HTML document was not declared. The document will render with garbled text in some browser configurations if the document contains characters from outside the US-ASCII range. The character encoding of the page must be declared in the document or in the transfer protocol. @ file:///Users/bigdsweetz/Documents/FullSail/Web%20Programming%20Fundamentals/Week%201/First%20Git%20Project/Associating%20JS/index.html
    
    This was caused in Firefox whenever I would try and run my code for the class lessons. I learned that Firefox need one more line of code before it will run with no problems
    
    I searched through Google and came across an answer for my problem at https://github.com/webpack/webpack-dev-server/issues/1. The answer is to input a meta charset that will solve this issue.
    
    For whatever reason, Firefox has not fixed this and they are currently working on resolving this. IT has been an ongoing issue since 2010.

--- Associating JS/js/script.js
+++ Associating JS/js/script.js
@@ -1,5 +1,5 @@
 //JAVASCRIPT
-
+/*
 var yearBorn = 1955;
 console.log(yearBorn);
 alert("The meaning a life");
@@ -14,3 +14,61 @@ var phrase = " I \n don't \n know";
 var muppetNames = ["1","2","3", firstName ];
 
 console.log(muppetNames[3]);
+
+//Expressions
+
+var a = 2; //defines a
+a = a + 3; //adds a + 3
+console.log(a);
+
+
+//Expressions - Age Example
+var yearBorn = 1981
+age = 2013 - yearBorn;
+alert(age);
+
+//Arithmetic Operators
+
+var height = prompt("Please enter the height.");
+var width = prompt("Please enter the width.");
+var area = width * height;
+alert("The area of your rectangle is " + area);
+
+//Modulo Operator
+
+var remainder = 32 / 10;
+//remainder 32 % 10 gives you the remainder of a division function.
+console.log(remainder);
+
+//Order of Operations PEMDAS
+
+var quiz1 = 87;
+var quiz2 = 100;
+var quiz3 = 60;
+var quiz4 = 80;
+
+//the average is the sum of the items divided by the number of items
+var average = (quiz1 + quiz2 + quiz3 + quiz4)/4;
+console.log(average);
+
+//Expressions - Expressions with Arrays
+
+var orangeBins = [234, 567, 883];
+
+var total = orangeBins[0] + orangeBins[1] + orangeBins [2];
+console.log(total);
+
+
+//Expressions Concatenating Strings
+
+var firstName = prompt("Please enter your first name.");
+var lastName = prompt("Please enter your last name.");
+alert(firstName + " " + lastName);
+*/
+
+//Expressions - Assignment Operators
+ var a = 3;
+a += 4; //a = a + 4
+// a++ is the same as a = a + 1
+// a-- is the same as a = a - 1
+console.log(a)
