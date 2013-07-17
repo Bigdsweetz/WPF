f0b0e525448136d26a50f507210b2324c69da68b
Bigdsweetz <bigdsweetz@gmail.com>
2013-07-17 04:43:18 -0400

    This is my equation for two things that are for the industry. One of the equations tell you the perfect ratio a page should be and the second equation tells you how to slice the page into 3rds or a 9 x 9 square for aesthetic value.

---
+++ Homework Week 2/Expressions_Industry/js/script.js
@@ -0,0 +1,22 @@
+/* Douglas DJay Sweeting
+ July 16 2013
+ Expression Worksheet
+ The Divine Proportion and the rule of Thirds
+ Expressions Industry*/
+
+
+//X px / 1.617 = y px is the devin ratio
+var pixelWidth = prompt ("What is the total width in pixels");  //user will input the width in pixels
+var pixelHeight = prompt ("What is the total height in pixels");//user will input the height in pixels
+var divineProportion = parseInt(pixelWidth / 1.618); //This computes the golden ratio
+var divineAnswer = pixelWidth - divineProportion;
+//The below shows the user what the divine ratio should be.
+alert("Your Divine Proportion from " + pixelWidth + " is " + divineProportion + " pixels wide " + divineAnswer + " pixels wide " );
+
+//according to the rule of 3rds
+var thirdWidth = pixelWidth / 3; //This will take the pixel width and cut it into 3rds
+var thirdHeight = pixelHeight / 3; //This will take the pixel length and cut it into 3rds
+var thirds = (thirdHeight + thirdWidth) / 2;
+alert("According to the rules of Thrids, the pixel of each square should be " + parseInt(thirdWidth) + " by " + parseInt(thirds) + " pixels.");
+
+
