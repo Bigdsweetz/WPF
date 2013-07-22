171a432bc932902ed77263062908addcd9a67dd3
Bigdsweetz <bigdsweetz@gmail.com>
2013-07-22 16:05:27 -0400

    This is my first swing at the homework due on Thursday.

---
+++ Week 3/Conditionals_worksheet/js/script.js
@@ -0,0 +1,57 @@
+/* Douglas DJay Sweeting
+ July 16 2013
+ Expression Worksheet
+ Expressions with Conditionalsy*/
+
+
+//Last Chance for Gas
+
+
+
+var gasTank;  // This is the reading of the gas tank
+var mpg = 20; // This is how efficient the car is with gas
+var maxGallans = 21; //This is the max amount of gas this vehicle holds
+var totalMiles = gasTank * mpg; // This shows
+var milesToZero = totalMiles - 200;
+
+if totalMiles > 200
+    You can do it!
+    else
+    You only have (totalMiles) of gas.
+
+
+
+//Grade Letter Calculator
+
+var grade;
+
+if garde 95 - 100 A+
+You received a (grade) which results in an A+
+else if 90 - 94 A
+You received a (grade) which results in an A
+else if 89 - 85 B+
+You received a (grade) which results in a B+
+else if 84 - 80 B
+You received a (grade) which results in a B
+else if 79 - 76 C+
+You received a (grade) which results in a C+
+else if 75 - 73 C
+You received a (grade) which results in a C
+else if 72 - 70 D
+You received a (grade) which results in a D
+else 0 - 69 FAIL
+You received a (grade) which results is a failure
+
+
+//Tire Pressure I
+
+var tirePressure = [0 , 1, 2, 3]; //Initializing an array.
+tirePressure [0] = prompt ("What is the PSI of your right front tire?"); //Input for front right tire
+tirePressure [1] = prompt ("What is the PSI of your left front tire?"); //Input for front left tire
+tirePressure [2] = prompt ("What is the PSI of your right rear tire?"); //Input for back right tire
+tirePressure [3] = prompt ("What is the PSI of your left rear tire?"); //Input for back left tire
+
+if leftfront == rightfront && leftRear == rightRear)
+    you pass
+else
+you fail
