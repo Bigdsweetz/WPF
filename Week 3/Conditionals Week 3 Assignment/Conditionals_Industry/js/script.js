2540f639599bd3ae6439dfd51b39ea4f4d111a1c
Bigdsweetz <bigdsweetz@gmail.com>
2013-07-23 12:42:13 -0400

    This is my first cut on the Industry based homework. I have laid down what I would like to do as far as the project is concerned.

---
+++ Week 3/Conditionals Week 3 Assignment/Conditionals_Industry/js/script.js
@@ -0,0 +1,11 @@
+/* Douglas DJay Sweeting
+ July 22 2013
+ Expression Worksheet
+ Conditional Industryy*/
+
+/* This will be the cost of running a web design business
+    The user will in put how many hours a week they work and
+    how much they get paid by the hour.  Business expenses will
+    be pre defined by me. The user will then be told if they make
+    enough money to support the business or if they need to work
+    more. */
