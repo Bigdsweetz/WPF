b4ade62ceb290aae6b3ad9b10a53e394d4072317
Bigdsweetz <bigdsweetz@gmail.com>
2013-07-23 12:42:44 -0400

    This is my first cut on the Personal based homework. I have laid down what I would like to do as far as the project is concerned.

---
+++ Week 3/Conditionals Week 3 Assignment/Conditionals_Personal/js/script.js
@@ -0,0 +1,17 @@
+/* Douglas DJay Sweeting
+ July 22 2013
+ Expression Worksheet
+ EConditionals Personal*/
+
+/*This will be a budget. The Variables will be
+    Money in
+    Gas/Electricity
+    Water
+    Cable/Internet
+    Cell Phone
+    Food
+    Gas
+
+    Also included will be how much money is needed for a trip which the user will input
+    After going through the monthly bill the output will tell if the user will be able to go
+    on the trip they  planned or if they are going to come up short. */
