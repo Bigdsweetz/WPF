ca02d3f8fa1120ec275d721a48628fc5d58ea52e
Bigdsweetz <bigdsweetz@gmail.com>
2013-07-23 12:50:00 -0400

    I'll be honest. I just wanted an excuse to be able to make StarTrek and Star Wars references. This project will tell you how fast you will get to your destination.

---
+++ Week 3/Conditionals Week 3 Assignment/Conditionals_Wacky/js/script.js
@@ -0,0 +1,11 @@
+/* Douglas DJay Sweeting
+ July 22 2013
+ Expression Worksheet
+ Conditional Wacky*/
+
+/* This I'm having fun with. With this, I am trying to figure out if I can make the drive to somewhere
+    I will have the user input a distance and from there I will also have the user input speed. I'm going to force the
+    user to select a pre defined location, but they will be able to input the speed upon which they would like to get
+    there. After that I will have the user input their age and let them know if they would live to see the end of the
+    trip successfully. As a side note, I will try and figure out the relative speed of StarTrek and the Millennium Falcon
+    as to give the user a possible chance of actually getting to their location alive. */
