dec1831c4f0f97f72f004a1d94b38c6d4dcddaf8
Bigdsweetz <bigdsweetz@gmail.com>
2013-07-25 17:56:53 -0400

    I went back and added some checks to make sure the user was putting something in each question.

--- Week 3/Conditionals Week 3 Assignment/Conditionals_Personal/js/script.js
+++ Week 3/Conditionals Week 3 Assignment/Conditionals_Personal/js/script.js
@@ -17,16 +17,55 @@
     on the trip they  planned or if they are going to come up short. */
 
 var moneyIn = prompt ("How much a month are you making?");  //User inputs monthly salary
+if (moneyIn == null|| moneyIn ==""){  //This verified if the user actually inputted one of the selection or skipped
+    console.log("No input for money made!");
+}else{
+    console.log("")
+}
 var gasEle = prompt ("How much is your electric bill monthly?"); //User inputs monthly electricity bill
+if (gasEle == null|| gasEle ==""){  //This verified if the user actually inputted one of the selection or skipped
+    console.log("No input for electric bill");
+}else{
+    console.log("")
+}
 var water = prompt ("What is your water bill monthly?"); //User inputs monthly water bill
-var cable = prompt ("How much do you pay for cable?"); //User inputs monthly cable bill
-var cell = prompt ("How about a cell phone?"); //User inputs monthly cell phone bill
-var food = prompt ("How much do you spend on food?"); //User inputs monthly food
-var gas = prompt ("How much do you spend on gas monthly?"); //User inputs monthly gas usage
 
+if (water == null|| water ==""){  //This verified if the user actually inputted one of the selection or skipped
+    console.log("No input for water bill");
+}else{
+    console.log("")
+}
+var cable = prompt ("How much do you pay for cable?"); //User inputs monthly cable bill
+if (cable == null|| cable ==""){  //This verified if the user actually inputted one of the selection or skipped
+    console.log("No input for cable");
+}else{
+    console.log("")
+}
+var cell = prompt ("How about a cell phone?"); //User inputs monthly cell phone bill
+if (cell== null|| cell ==""){  //This verified if the user actually inputted one of the selection or skipped
+    console.log("No input for cell phone");
+}else{
+    console.log("")
+}
+var food = prompt ("How much do you spend on food?"); //User inputs monthly food
+if (food == null|| food ==""){  //This verified if the user actually inputted one of the selection or skipped
+    console.log("No input for food.");
+}else{
+    console.log("")
+}
+var gas = prompt ("How much do you spend on gas monthly?"); //User inputs monthly gas usage
+if (gas == null|| gas ==""){  //This verified if the user actually inputted one of the selection or skipped
+    console.log("No input for gas...must ride a bike");
+}else{
+    console.log("")
+}
 //User inputs how much money they are going to need for their trip
 var tripMoney = prompt ("How much do you need for your trip?");
-
+if (tripMoney == null|| tripMoney ==""){  //This verified if the user actually inputted one of the selection or skipped
+    console.log("Guess we're just not going on a trip eh?");
+}else{
+    console.log("")
+}
 
 //simple equation to figure out left over money
 var result = moneyIn - gasEle - water - cable - cell -food - gas;
