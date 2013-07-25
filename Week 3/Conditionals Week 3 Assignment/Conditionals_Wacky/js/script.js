30541eec76317b81d241a6b6f41d05af44fba431
Bigdsweetz <bigdsweetz@gmail.com>
2013-07-25 17:09:38 -0400

    I've gone back and added comments to each of the sections.

--- Week 3/Conditionals Week 3 Assignment/Conditionals_Wacky/js/script.js
+++ Week 3/Conditionals Week 3 Assignment/Conditionals_Wacky/js/script.js
@@ -26,21 +26,21 @@ var japan = 6737;
 var beijing = 6837;
 
 var time;
-//Math = Vehicle/Distance
 
+//This is where the user inputted their choice of Destination
 var choiceA = prompt("Where would you like to go from New York? \n" +
     "1.)California\n" +
     "2.)United Kingdom\n" +
     "3.)Japan\n" +
     "4.)Beijing");
 
-if (choiceA == null|| choiceA ==""){
+if (choiceA == null|| choiceA ==""){  //This verified if the user actually inputted one of the selction or skipped
 console.log("No choice was selected for destination!");
 }else{
     console.log("a number not on the list was picked")
 }
 
-
+//This is where the user selected their mode of transportation
 var choiceB = prompt("How are you getting there? \n" +
     "1.)running\n" +
     "2.)walking\n" +
@@ -52,157 +52,158 @@ var choiceB = prompt("How are you getting there? \n" +
     "8.)Space Shuttle\n" +
     "9.)Thrust SSC");
 
-if (choiceB == null|| choiceB =="") {
+if (choiceB == null|| choiceB =="") {   //This verified if the user actually inputted one of the selction or skipped
     console.log("No choice was selected for means of transportation!");
 }
     else{
     console.log("A number not on the list was picked")
 }
 
-
+//Math = Distance/Vehicle
+//Below I did the mathmatical equation for each combo of choices
 if (choiceA == 1 && choiceB ==1){
-    time = california/running
+    time = california/running;
     console.log("It will take you " + time + " hours to get to California");
 
 }   else if(choiceA == 1 && choiceB ==2){
-    time = california/walking
+    time = california/walking;
     console.log("It will take you " + time + " hours to get to California");
 
 }    else if(choiceA == 1 && choiceB ==3){
-    time = california/biking
+    time = california/biking;
     console.log("It will take you " + time + " hours to get to California");
 
 }    else if(choiceA == 1 && choiceB ==4){
-    time = california/spiritOfAmerica
+    time = california/spiritOfAmerica;
     console.log("It will take you " + time + " hours to get to California");
 
 }    else if(choiceA == 1 && choiceB ==5){
-    time = california/seaplane
+    time = california/seaplane;
     console.log("It will take you " + time + " hours to get to California");
 
 }    else if(choiceA == 1 && choiceB ==6){
-    time = california/boeing
+    time = california/boeing;
     console.log("It will take you " + time + " hours to get to California");
 
 }    else if(choiceA == 1 && choiceB ==7){
-    time = california/concord
+    time = california/concord;
     console.log("It will take you " + time + " hours to get to California");
 
 }    else if(choiceA == 1 && choiceB ==8){
-    time = california/spaceShuttle
+    time = california/spaceShuttle;
     console.log("It will take you " + time + " hours to get to California");
 
 }    else if(choiceA == 1 && choiceB ==9){
-    time = california/thrust
+    time = california/thrust;
     console.log("It will take you " + time + " hours to get to California");
 
 }    else if (choiceA == 2 && choiceB ==1){
-    time = unitedKingdom/running
+    time = unitedKingdom/running;
     console.log("It will take you " + time + " hours to get to the United Kingdom");
 
 }   else if(choiceA == 2 && choiceB ==2){
-    time = unitedKingdom/walking
+    time = unitedKingdom/walking;
     console.log("It will take you " + time + " hours to get to the United Kingdom");
 
 }    else if(choiceA == 2 && choiceB ==3){
-    time = unitedKingdom/biking
+    time = unitedKingdom/biking;
     console.log("It will take you " + time + " hours to get to the United Kingdom");
 
 }    else if(choiceA == 2 && choiceB ==4){
-    time = unitedKingdom/spiritOfAmerica
+    time = unitedKingdom/spiritOfAmerica;
     console.log("It will take you " + time + " hours to get to the United Kingdom");
 
 }    else if(choiceA == 2 && choiceB ==5){
-    time = unitedKingdom/seaplane
+    time = unitedKingdom/seaplane;
     console.log("It will take you " + time + " hours to get to the United Kingdom");
 
 }    else if(choiceA == 2 && choiceB ==6){
-    time = unitedKingdom/boeing
+    time = unitedKingdom/boeing ;
     console.log("It will take you " + time + " hours to get to the United Kingdom");
 
 }    else if(choiceA == 2 && choiceB ==7){
-    time = unitedKingdom/concord
+    time = unitedKingdom/concord;
     console.log("It will take you " + time + " hours to get to the United Kingdom");
 
 }    else if(choiceA == 2 && choiceB ==8){
-    time = unitedKingdom/spaceShuttle
+    time = unitedKingdom/spaceShuttle;
     console.log("It will take you " + time + " hours to get to the United Kingdom");
 
 }    else if(choiceA == 2 && choiceB ==9){
-    time = unitedKingdom/thrust
+    time = unitedKingdom/thrust;
     console.log("It will take you " + time + " hours to get to the United Kingdom");
 
 }   else if (choiceA == 3 && choiceB ==1){
-    time = japan/running
+    time = japan/running;
     console.log("It will take you " + time + " hours to get to Japan");
 
 }   else if(choiceA == 3 && choiceB ==2){
-    time = japan/walking
+    time = japan/walking;
     console.log("It will take you " + time + " hours to get to Japan");
 
 }    else if(choiceA == 3 && choiceB ==3){
-    time = japan/biking
+    time = japan/biking ;
     console.log("It will take you " + time + " hours to get to Japan");
 
 }    else if(choiceA == 3 && choiceB ==4){
-    time = japan/spiritOfAmerica
+    time = japan/spiritOfAmerica;
     console.log("It will take you " + time + " hours to get to Japan");
 
 }    else if(choiceA == 3 && choiceB ==5){
-    time = japan/seaplane
+    time = japan/seaplane;
     console.log("It will take you " + time + " hours to get to Japan");
 
 }    else if(choiceA == 3 && choiceB ==6){
-    time = japan/boeing
+    time = japan/boeing;
     console.log("It will take you " + time + " hours to get to Japan");
 
 }    else if(choiceA == 3 && choiceB ==7){
-    ctime = japan/concord
+    ctime = japan/concord;
     console.log("It will take you " + time + " hours to get to Japan");
 
 }    else if(choiceA == 3 && choiceB ==8){
-    time = japan/spaceShuttle
+    time = japan/spaceShuttle;
     console.log("It will take you " + time + " hours to get to Japan");
 
 }    else if(choiceA == 3 && choiceB ==9){
-    time = japan/thrust
+    time = japan/thrust;
     console.log("It will take you " + time + " hours to get to Japan");
 
 }    else if (choiceA == 4 && choiceB ==1){
-    time = beijing/running
+    time = beijing/running;
     console.log("It will take you " + time + " hours to get to Beijing");
 
 }   else if(choiceA == 4 && choiceB ==2){
-    time = beijing/walking
+    time = beijing/walking ;
     console.log("It will take you " + time + " hours to get to Beijing");
 
 }    else if(choiceA == 4 && choiceB ==3){
-    time = beijing/biking
+    time = beijing/biking;
     console.log("It will take you " + time + " hours to get to Beijing");
 
 }    else if(choiceA == 4 && choiceB ==4){
-    time = beijing/spiritOfAmerica
+    time = beijing/spiritOfAmerica;
     console.log("It will take you " + time + " hours to get to Beijing");
 
 }    else if(choiceA == 4 && choiceB ==5){
-    time = beijing/seaplane
+    time = beijing/seaplane;
     console.log("It will take you " + time + " hours to get to Beijing");
 
 }    else if(choiceA == 4 && choiceB ==6){
-    time = beijing/boeing
+    time = beijing/boeing;
     console.log("It will take you " + time + " hours to get to Beijing");
 
 }    else if(choiceA == 4 && choiceB ==7){
-    time = beijing/concord
+    time = beijing/concord ;
     console.log("It will take you " + time + " hours to get to Beijing");
 
 }    else if(choiceA == 4 && choiceB ==8){
-    time = beijing/spaceShuttle
+    time = beijing/spaceShuttle ;
     console.log("It will take you " + time + " hours to get to Beijing");
 
 }    else if(choiceA == 4 && choiceB ==9){
-    time = beijing/thrust
+    time = beijing/thrust ;
     console.log("It will take you " + time + " hours to get to Beijing");
 
-}else
+}else  //This is my last line. I used this to make sure my loop worked
     console.log("You should not see this");
