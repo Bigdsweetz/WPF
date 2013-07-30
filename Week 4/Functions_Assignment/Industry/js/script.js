840ed24fed3b2e30a5e8e39db662ec2cab04c5b2
Bigdsweetz <bigdsweetz@gmail.com>
2013-07-29 20:15:07 -0400

    First go through of what I want to do with my Industry Homework.

---
+++ Week 4/Functions_Assignment/Industry/js/script.js
@@ -0,0 +1,26 @@
+/* Douglas DJay Sweeting
+ July 29 2013
+ Expression Worksheet
+ Industry Function*/
+
+
+/*A user wants to have people
+    input what kind of work
+    they want done and also
+    wants to be able to give them
+    an estimate. */
+
+//Lawn Work
+
+//How much to get rid of trees
+
+//Price of trees and how many trees
+
+
+//How much to cut grass?
+//How big the lawn and is it front and back
+
+//How much for yard clean up
+//Front and back?
+
+//overall price
