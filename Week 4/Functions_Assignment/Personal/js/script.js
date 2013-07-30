0a0d3ba160388b0344082579a04106d01d76952d
Bigdsweetz <bigdsweetz@gmail.com>
2013-07-29 20:15:59 -0400

    I laid out what I would like to do with my Personal Function. This will be one of me bringing my dogs to the vet. How much this will set me back.

---
+++ Week 4/Functions_Assignment/Personal/js/script.js
@@ -0,0 +1,22 @@
+/* Douglas DJay Sweeting
+ July 22 2013
+ Expression Worksheet
+ Personal Function*/
+
+
+/* I want to take the dog to the vet and I
+    need to know how much I'm going to spend*/
+
+//Which dog am I taking
+
+//How much do they weigh?
+
+//Shots? If so how many?
+
+//Clipping nails?
+
+//Grooming?
+
+//Teeth?
+
+//Overall price
