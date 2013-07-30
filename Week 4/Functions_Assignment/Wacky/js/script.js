c0d1b025b7d654b4e697db676ba7d223b4160fc8
Bigdsweetz <bigdsweetz@gmail.com>
2013-07-30 14:56:57 -0400

    This is my completed week 4 activity. This encompasses everything that I've done for the week 4 project.

--- Week 4/Functions_Assignment/Wacky/js/script.js
+++ Week 4/Functions_Assignment/Wacky/js/script.js
@@ -17,69 +17,74 @@ var currency = prompt("Please enter what currency you are using?\n" +
     "5 European Euro\n");
 
 
-//What currency are you using
-var amount = prompt("Please enter how much you plan on entering?"); //United States Dollar
+//Has the user input how much they plan on saving a month
+var amount = prompt("Please enter how much you plan on entering?"); //Default will be dollars
 
 
 var totalUSD = USD(amount);
+
     function USD() {
     var goal = 10000 / amount;
-    return goal;//The return for this function
+    return goal;//The return for this function in United States dollars
 }
 var totalYEN = YEN(amount);
+
     function YEN() {
-    var yenAmt = amount * .98; //Japanese Yen
+    var yenAmt = amount * .98; //This converts to Japanese Yen
     var goal = 10000/yenAmt;
     return goal;
     }
 
 var totalWON = WON (amount);
+
     function WON() {
-    var wonAmt = amount * 1112.14; //Korean currency
+    var wonAmt = 1112.14/ amount; //This converts to Korean currency
     var goal = 10000/wonAmt;
     return goal;
     }
 
 var totalGBP = GBP(amount);
+
     function GBP() {
-    var gbpAmt = amount * .65 ; //Great British Pound
+    var gbpAmt = amount * .65 ; //This converts to Great British Pound
     var goal = 10000/gbpAmt;
     return goal;
     }
 
 var totalEUR = EUR(amount);
+
     function EUR() {
-    var eurAmt = amount * .75; //European Euro
+    var eurAmt = amount * .75; //This converts to European Euro
     var goal = 10000/eurAmt;
     return goal;
     }
 
 
-/*
-if (currency = 1){
-    function USD(amount){
+
+//console.log(totalYEN)
+
+
+if (currency == 1){
+    {
         console.log("It will take " + totalUSD + " months to reach your goal.");
     }
-}else if (currency = 2){
-    function YEN(USD){
+}else if (currency == 2){
+    {
         console.log("It will take " + totalYEN + " months to reach your goal.");
     }
-}else if (currency = 3){
-    function WON(USD){
+}else if (currency == 3){
+    {
         console.log("It will take " + totalWON + " months to reach your goal.");
     }
-}else if (currency = 4){
-    function GBP(USD){
+}else if (currency == 4){
+    {
         console.log("It will take " + totalGBP + " months to reach your goal.");
     }
-}else if (currency = 5){
-    function EUR(USD){
+}else if (currency == 5){
+    {
         console.log("It will take " + totalEUR + " months to reach your goal.");
     }
+} else {
+    console.log("The user hit a number not on the list or did not select a number");
+
 }
- */
-/*Create the if else part as functions and then call them in side of a giant Do While loop asking if you want to run
- * the program again. */
-
-
-//would you like to run the program again?
