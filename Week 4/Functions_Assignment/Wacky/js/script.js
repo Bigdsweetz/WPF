dd33f157f6f3a783d342fa3cc16fd7506dac5a03
Bigdsweetz <bigdsweetz@gmail.com>
2013-07-29 20:17:23 -0400

    This is my first attempt at my wacky file. I will be doing a question of how long it will take to reach 10,000 USD, butI will allow the user to input multiple currencies and how much they are willing to invest a month.

---
+++ Week 4/Functions_Assignment/Wacky/js/script.js
@@ -0,0 +1,16 @@
+/* Douglas DJay Sweeting
+ July 29 2013
+ Expression Worksheet
+ Wacky*/
+
+
+/* How long would it take to save 10,000 USD.
+    Also, what currency will you be using
+    How much do you want to put away per month? */
+
+
+//How much are you going to save a month
+
+//What currency are you using
+
+//would you like to run the program again?
