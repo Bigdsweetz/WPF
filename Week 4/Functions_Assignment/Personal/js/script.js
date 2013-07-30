86dc7c8343c1a1c97f4e54db68879ab0d877af89
Bigdsweetz <bigdsweetz@gmail.com>
2013-07-30 13:06:09 -0400

    This is my first go at the personal project. This will let me know how much it will cost to take one of my dogs to the vet.

--- Week 4/Functions_Assignment/Personal/js/script.js
+++ Week 4/Functions_Assignment/Personal/js/script.js
@@ -8,15 +8,38 @@
     need to know how much I'm going to spend*/
 
 //Which dog am I taking
+var choice = prompt("Which dog are you taking to the vet?\n" +
+    "1. Cujo\n" +
+    "2. Roxy\n");
+
+choice = (choice == 1) ? "Cujo" : "Roxy";
 
 //How much do they weigh?
+var weight = prompt("How much does " + choice + " weigh?");
+weight = (weight <= 70) ? 55 : 75;
 
 //Shots? If so how many?
-
+var shots = prompt("Does " + choice + " need shots?\n" +
+    "1 for yes\n" +
+    "2 for no.");
+shots = (shots == 1) ? 100 : 0;
 //Clipping nails?
-
+var clipping = prompt("Would you like to have " + choice + "'s nails cut?\n" +
+    "1 for yes\n" +
+    "2 for no.");
+clipping = (clipping == 1) ? 25 : 0;
 //Grooming?
-
+var grooming = prompt("Would you like to have " + choice + " groomed?\n" +
+    "1 for yes\n" +
+    "2 for no.");
+grooming = (grooming == 1) ? 15 : 0;
 //Teeth?
+var teeth = prompt("Do you want " + choice + " teeth cleaned?\n" +
+    "1 for yes\n" +
+    "2 for no.");
+teeth = (teeth == 1) ? 10 : 0;
 
 //Overall price
+var overallPrice = weight + shots + clipping + grooming + teeth;
+    console.log("The price for this visit will be $" + overallPrice + " dollars.");
+
