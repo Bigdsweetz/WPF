e59bc37e89b6dd6102ffd16fe931c69078a51001
Bigdsweetz <bigdsweetz@gmail.com>
2013-07-30 14:51:43 -0400

    I literally did this in one shot. I wrote this all out once and then ran the program and it worked the first time. I went back and looked at what I had done successfully in other places and used that as a reference as to what to do for this.

--- Week 4/Functions_Assignment/Industry/js/script.js
+++ Week 4/Functions_Assignment/Industry/js/script.js
@@ -12,15 +12,42 @@
 
 //Lawn Work
 
-//How much to get rid of trees
+function yardWork(){
+     var trees = prompt("Do you need trees cut down?\n" +
+         "1. Yes\n" +
+         "2. No");
 
-//Price of trees and how many trees
+     if (trees == 1){
+         var howMany = prompt("How many trees do you want cut down?");
+         var price = 50 * howMany;
+     }
+    else{
+         var price = 0;
+     }
+    var lawn = prompt("Do you want your lawn cut? \n" +
+        "If so how big is your lawn?\n" +
+        "Q. No Thanks");
+    if (lawn >= 120){
+        var lawnPrice = 200;
+    }else if ((lawn > 120) && (lawn <= 450)){
+        var lawnPrice = 400;
+    }else if (lawn >450 ){
+        var lawnPrice = 500;
+    }  else {
+        var lawnPrice = 0;
+    }
 
+    var clean = prompt("Would you like us to clean your yard?\n" +
+        "1. Yes\n" +
+        "2. No");
+    if (clean == 1){
+        var cleanPrice = 150;
+    }
+        else{
+            var cleanPrice = 0;
+        }
+    var overallPrice = price + lawnPrice + cleanPrice;
+    console.log(overallPrice);
+}
 
-//How much to cut grass?
-//How big the lawn and is it front and back
-
-//How much for yard clean up
-//Front and back?
-
-//overall price
+yardWork()
